05803881bc049f257d7482cc13206e969baf11a8
darkobits <joshua@frontlawn.net>
2020-01-16 15:56:07 -0800

    fix: Gracefully handle storage unavailability.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,29 +1,26 @@
 import bytes from 'bytes';
 
-/**
- * Capitalizes the first letter of the provided string.
- */
-export function capitalizeFirst(str: string) {
-  return str.substr(0, 1).toUpperCase() + str.substr(1);
-}
-
 
 /**
  * Prints current storage usage and quotas to the console.
  */
 export async function printStorageUsage() {
-  if (process.env.NODE_ENV !== 'development') {
-    return;
-  }
+  if (process.env.NODE_ENV === 'development') {
+    const storageEstimate = navigator.storage && await navigator.storage.estimate();
 
-  const storageEstimate = await navigator.storage.estimate();
+    // The navigator.storage object is undefined when using private mode in
+    // Safari.
+    if (!storageEstimate) {
+      return;
+    }
 
-  // @ts-ignore
-  const idbUsage = storageEstimate?.usageDetails?.indexedDB;
-  const quota = storageEstimate.quota;
+    // @ts-ignore (usageDetails is not typed correctly).
+    const idbUsage = storageEstimate?.usageDetails?.indexedDB;
+    const quota = storageEstimate.quota;
 
-  if (idbUsage && quota) {
-    const percentageUsed = ((idbUsage / quota) * 100).toFixed(2);
-    console.debug(`IndexedDB is currently using ${bytes(idbUsage)} of data, or ${percentageUsed}% of the ${bytes(quota)} quota.`);
+    if (idbUsage && quota) {
+      const percentageUsed = ((idbUsage / quota) * 100).toFixed(2);
+      console.debug(`IndexedDB is currently using ${bytes(idbUsage)} of data, or ${percentageUsed}% of the ${bytes(quota)} quota.`);
+    }
   }
 }
