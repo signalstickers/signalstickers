7f5b70c7eece083350d09a97e353009a7ec1a875
darkobits <joshua+github@frontlawn.net>
2020-09-25 17:37:32 -0700

    refactor: Dynamically import webp-hero.

--- src/components/pack/Sticker.tsx
+++ src/components/pack/Sticker.tsx
@@ -76,7 +76,7 @@ const StickerComponent: React.FunctionComponent<Props> = ({packId, packKey, stic
    * [Effect] Load sticker data and emoji for the sticker indicated in our
    * props.
    */
-  useAsyncEffect(async () => {
+  useAsyncEffect(async isMounted => {
     const [
       emojiResult,
       stickerResult
@@ -85,8 +85,17 @@ const StickerComponent: React.FunctionComponent<Props> = ({packId, packKey, stic
       getConvertedStickerInPack(packId, packKey, stickerId)
     ]);
 
-    setEmoji(emojiResult);
-    setStickerSrc(stickerResult);
+    // The user may have navigated away from this page before all stickers have
+    // loaded, causing the component to un-mount before the above Promises
+    // resolve. Calling setState on an un-mounted component is a no-op, but
+    // triggers a warning in development mode. Fortunately, use-async-effect
+    // provides an isMounted predicate we can call to determine if we should
+    // proceed with updating a component's state after potentially lengthy async
+    // operations.
+    if (isMounted()) {
+      setEmoji(emojiResult);
+      setStickerSrc(stickerResult);
+    }
   }, [
     packId,
     packKey,
