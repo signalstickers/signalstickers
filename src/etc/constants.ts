4916b4aa3b214a56ffcb596f749acc256a950c79
Romain Ricard <romain.ricard@mines-ales.org>
2021-07-03 17:51:17 +0200

    fix: Change ping URL

--- src/etc/constants.ts
+++ src/etc/constants.ts
@@ -36,7 +36,7 @@ const API_BASE_URL = 'https://api.signalstickers.com/v1';
 const API_URL_CONTRIBUTIONREQUEST = `${API_BASE_URL}/contributionrequest/`;
 const API_URL_CONTRIBUTE = `${API_BASE_URL}/contribute/`;
 const API_URL_PACKS = `${API_BASE_URL}/packs/`;
-const API_URL_PACKS_PING = `${API_BASE_URL}/packs/ping/`;
+const API_URL_PACKS_PING = `${API_BASE_URL}/ping/`;
 const API_URL_STATUS = `${API_BASE_URL}/packs/status/`;
 
 export { API_BASE_URL, API_URL_PACKS, API_URL_PACKS_PING, API_URL_CONTRIBUTIONREQUEST, API_URL_CONTRIBUTE, API_URL_STATUS };
